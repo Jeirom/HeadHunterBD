931cd222486324b051f7184ea99d8fdebc873450
Daniil <jeiromkrizhanovskiyd@gmail.com>
2024-12-02 19:00:54 +0500

    Создан: utils.py, api_headhunter.py, DBManager - DBManager.py Написана структура DBManager.py, написан код в api_headhunter.py

---
+++ DBManager/DBManager.py
@@ -0,0 +1,30 @@
+class DBManager:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_companies_and_vacancies_count():
+        """Получает список всех компаний и количество вакансий у каждой компании."""
+        pass
+
+    @staticmethod
+    def get_all_vacancies():
+        """Получает список всех вакансий с указанием названия компании,
+        названия вакансии и зарплаты и ссылки на вакансию."""
+        pass
+
+    @staticmethod
+    def get_avg_salary():
+        """Получает среднюю зарплату по вакансиям."""
+        pass
+
+    @staticmethod
+    def get_vacancies_with_higher_salary():
+        """Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям."""
+        pass
+
+    @staticmethod
+    def get_vacancies_with_keyword():
+        """Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python."""
+        pass
